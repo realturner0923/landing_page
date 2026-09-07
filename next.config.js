62d9e57a20f30713ede1947de505b83fa6b35873
realturner0923 <realturner0923@outlook.com>
2026-09-07 09:31:22 -0700

    Configure Next.js for static export
    Export the Next.js app as static files for GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+
+  // Export the Next.js app as static files for GitHub Pages
+  output: 'export',
 });
